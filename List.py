a3d296909382ab8980a30d534fc7da1002113751
Ali Hamza <ali.mhamza06@gmail.com>
2025-08-17 03:15:12 +0400

    Add some test methods for list (List) objects.

--- List.py
+++ List.py
@@ -1,11 +1,55 @@
+from LoxCallable import LoxCallable
 from Error import RuntimeError
+
+class ListFunction(LoxCallable):
+    def __init__(self, mode):
+        self.mode = mode
+        self.instance = None
+    
+    def bind(self, instance):
+        self.instance = instance
+    
+    def check(self, expr, arguments):
+        pass
+
+    def call(self, interpreter, expr, arguments):
+        match self.mode:
+            case "printList":
+                self.l_print()
+            case "first":
+                return self.l_first()
+    
+    def l_print(self):
+        print(self.instance)
+    
+    def l_first(self):
+        return self.instance.array[0]
+
+    def arity(self):
+        match self.mode:
+            case "printList":
+                return 0
+            case "first":
+                return 0
+
+functions = ["printList", "first"]
 
 class List:
     def __init__(self, array: list):
         self.array = array
+        self.methods = dict()
+        for function in functions:
+            self.methods[function] = ListFunction(function)
 
-    def get():
-        pass
+    def get(self, name):
+        method = self.methods.get(name.lexeme, None)
+        if method != None:
+            import copy
+            func = copy.deepcopy(method)
+            func.bind(self)
+            return func
+        
+        raise RuntimeError(name, f"Undefined property or method '{name.lexeme}'.")
 
     def set():
         pass
@@ -23,15 +67,12 @@ class List:
 
 # To have a List() constructor function.
 # Alternative way of creating/declaring a list.
-from LoxCallable import LoxCallable
 class ListInit(LoxCallable):
     def bind(self, instance):
         self.instance = instance
     
     def check(self, arguments, expr):
-        if type(arguments[0]) == list:
-            return True
-        elif type(arguments[0]) == List:
+        if type(arguments[0]) == List:
             return True
         raise RuntimeError(expr.callee.name, 
                                        "Arguments do not match accepted parameter types.\n" \
@@ -39,10 +80,7 @@ class ListInit(LoxCallable):
 
     def call(self, interpreter, expr, arguments):
         if self.check(arguments, expr):
-            if type(arguments[0]) == list:
-                return List(arguments[0])
-            elif type(arguments[0]) == List:
-                return List(arguments[0].array)
+            return List(arguments[0].array)
 
     def arity(self):
         return 1
