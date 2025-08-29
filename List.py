774c6e4cd97ea91903e19546a9a49038e8b0e7c0
Ali Hamza <ali.mhamza06@gmail.com>
2025-08-29 14:53:27 +0400

    Complete list methods and correct arity method to fit default parameter changes.
    Also updated other parts of the code to accommodate default parameters.

--- List.py
+++ List.py
@@ -31,7 +31,10 @@ class ListFunction(LoxCallable):
                 return self.l_remove(expr, arguments[0])
             # Removes argument from list (if found) and returns it.
             case "delete":
-                self.l_delete(expr, arguments[0], arguments[1])
+                if len(arguments) == 2:
+                    self.l_delete(expr, arguments[0], arguments[1])
+                elif len(arguments) == 1:
+                    self.l_delete(expr, arguments[0])
                 return ()
 
             # Combines elements of the list into a single string.
@@ -44,15 +47,15 @@ class ListFunction(LoxCallable):
             # Applies some operation to all elements.
             # Operation need not return a value.
             case "forEach":
-                self.l_forEach(expr, arguments[0])
+                self.l_forEach(expr, interpreter, arguments[0])
                 return ()
             # Applies some operation to each element, returning a new list.
             # Operation must return a value (corresponding element in the new list).
             case "transform":
-                return self.l_transform(expr, arguments[0])
+                return self.l_transform(expr, interpreter, arguments[0])
             # Returns a new list with only the elements satisfying some predicate.
             case "filter":
-                return self.l_filter(expr, arguments[0])
+                return self.l_filter(expr, interpreter, arguments[0])
             # Returns a new list containing all non-list elements.
             # List elements are replaced with their contained elements, recursively.
             case "flat": 
@@ -72,14 +75,10 @@ class ListFunction(LoxCallable):
                 return self.l_indexLast(expr, arguments[0])
             # Returns whether any element satisfies a certain predicate.
             case "any":
-                return self.l_any(expr, arguments[0])
+                return self.l_any(expr, interpreter, arguments[0])
             # Returns whether all elements satisfy a certain predicate.
             case "all":
-                return self.l_all(expr, arguments[0])
-            # Returns a new list containing all the elements
-            # that satisfy a particular predicate.
-            case "collect":
-                return self.l_collect(expr, arguments[0])
+                return self.l_all(expr, interpreter, arguments[0])
             
             # Returns a reversed form of the original list.
             case "reverse":
@@ -87,17 +86,24 @@ class ListFunction(LoxCallable):
             # Returns a sorted form of the original list.
             # Can be done in ascending or descending order (Boolean parameter).
             case "sort":
-                return self.l_sort(expr, arguments[0])
+                if len(arguments) == 1:
+                    return self.l_sort(expr, arguments[0])
+                elif len(arguments) == 0:
+                    return self.l_sort(expr)
             # Returns whether or not the list is sorted.
             # Can check for ascending or descending order (Boolean parameter).
             case "sorted":
-                return self.l_sorted(expr, arguments[0])
+                if len(arguments) == 1:
+                    return self.l_sorted(expr, arguments[0])
+                elif len(arguments) == 0:
+                    return self.l_sorted(expr)
             # Returns a new list with index-based pairings from the instance
             # and argument.
             # Continues until one of the lists ends.
             case "pair":
                 return self.l_pair(expr, arguments[0])
             # Separates a list like the form produced by pair() into two lists.
+            # Returns a list containing the two lists.
             # Raises an error(?) if a non-pair element exists in the argument.
             case "separate":
                 return self.l_separate(expr)
@@ -160,18 +166,46 @@ class ListFunction(LoxCallable):
         uniqueArray = list(set(array))
         return List(uniqueArray)
 
-    def l_forEach(self, expr, operation):
-        pass
+    def l_forEach(self, expr, interpreter, operation):
+        array = self.instance.array
+        for element in array:
+            operation.call(interpreter, expr, [element])
 
-    def l_transform(self, expr, mapping):
-        pass
+    def l_transform(self, expr, interpreter, mapping):
+        array = self.instance.array
+        newArray = []
+        for element in array:
+            value = mapping.call(interpreter, expr, [element])
+            if type(value) == tuple:
+                raise RuntimeError(expr.leftParen, "Function argument must return a value.")
+            newArray.append(value)
+        return List(newArray)
 
-    def l_filter(self, expr, condition):
-        pass
+    def l_filter(self, expr, interpreter, condition):
+        array = self.instance.array
+        filterArray = []
+        for element in array:
+            predicate = condition.call(interpreter, expr, [element])
+            if predicate == True:
+                filterArray.append(element)
+            elif predicate == False:
+                pass
+            else:
+                raise RuntimeError(expr.leftParen, "Function argument must return a Boolean value.")
+        return List(filterArray)
+
+    def flatHelper(self, array):
+        flatArray = []
+        for element in array:
+            if type(element) == List:
+                flatArray += self.flatHelper(element.array)
+            else:
+                flatArray.append(element)
+        return flatArray
 
     def l_flat(self, expr):
-        pass
-
+        array = self.instance.array
+        return List(self.flatHelper(array))
 
     def l_contains(self, expr, element):
         return (element in self.instance.array)
@@ -195,14 +229,25 @@ class ListFunction(LoxCallable):
         else:
             return -1
 
-    def l_any(self, expr, condition):
-        pass
+    def l_any(self, expr, interpreter, condition):
+        array = self.instance.array
+        for element in array:
+            predicate = condition.call(interpreter, expr, [element])
+            if predicate == True:
+                return True
+            elif type(predicate) != bool:
+                raise RuntimeError(expr.leftParen, "Function argument must return a Boolean value.")
+        return False
 
-    def l_all(self, expr, condition):
-        pass
-    
-    def l_collect(self, expr, condition):
-        pass
+    def l_all(self, expr, interpreter, condition):
+        array = self.instance.array
+        for element in array:
+            predicate = condition.call(interpreter, expr, [element])
+            if predicate == False:
+                return False
+            elif type(predicate) != bool:
+                raise RuntimeError(expr.leftParen, "Function argument must return a Boolean value.")
+        return True
 
 
     def l_reverse(self, expr):
@@ -211,13 +256,13 @@ class ListFunction(LoxCallable):
         array.reverse()
         return List(array)
 
-    def l_sort(self, expr, ascending: bool):
+    def l_sort(self, expr, ascending: bool = True):
         import copy
         array = copy.deepcopy(self.instance.array)
         array.sort(reverse = not ascending)
         return List(array)
     
-    def l_sorted(self, expr, ascending: bool):
+    def l_sorted(self, expr, ascending: bool = True):
         array = self.instance.array
         for i in range(0, len(array) - 1):
             if ascending:
@@ -243,8 +288,15 @@ class ListFunction(LoxCallable):
         return List(pair)
 
     def l_separate(self, expr):
-        pass
-
+        # Already checked that each element in instance's array
+        # is a list containing exactly two elements.
+        array = self.instance.array
+        largeList = [[], []]
+        for element in array:
+            elemArray = element.array
+            largeList[0].append(elemArray[0])
+            largeList[1].append(elemArray[1])
+        return List(largeList)
 
     def l_sum(self, expr):
         array = self.instance.array
@@ -267,63 +319,61 @@ class ListFunction(LoxCallable):
     def arity(self):
         match self.mode:
             case "add":
-                return 1
+                return [1,1]
             case "insert":
-                return 2
+                return [2,2]
             case "pop":
-                return 0
+                return [0,0]
             case "remove":
-                return 1
+                return [1,1]
             case "delete":
-                return 2
+                return [1,2]
 
             case "join":
-                return 0
+                return [0,0]
             case "unique":
-                return 0
+                return [0,0]
             case "forEach":
-                return 1
+                return [1,1]
             case "transform":
-                return 1
+                return [1,1]
             case "filter":
-                return 1
+                return [1,1]
             case "flat":
-                return 0
+                return [0,0]
 
             case "contains":
-                return 1
+                return [1,1]
             case "duplicate":
-                return 0
+                return [0,0]
             case "index":
-                return 1
+                return [1,1]
             case "indexLast":
-                return 1
+                return [1,1]
             case "any":
-                return 1
+                return [1,1]
             case "all":
-                return 1
-            case "collect":
-                return 1
+                return [1,1]
 
             case "reverse":
-                return 0
+                return [0,0]
             case "sort":
-                return 1
+                return [0,1]
             case "sorted":
-                return 1
+                return [0,1]
             case "pair":
-                return 1
+                return [1,1]
             case "separate":
-                return 0
+                return [0,0]
 
             case "sum":
-                return 0
+                return [0,0]
             case "min":
-                return 0
+                return [0,0]
             case "max":
-                return 0
+                return [0,0]
             case "average":
-                return 0
+                return [0,0]
     
     # ------------------------------------------------------------
     def toString(self):
@@ -378,6 +428,8 @@ class ListInit(LoxCallable):
         self.instance = instance
     
     def check(self, arguments, expr):
+        if len(arguments) == 0:
+            return True
         obj = arguments[0]
         argType = type(obj)
         if (argType == List) or (argType == str):
@@ -391,19 +443,22 @@ class ListInit(LoxCallable):
 
     def call(self, interpreter, expr, arguments):
         if self.check(arguments, expr):
-            obj = arguments[0]
-            if type(obj) == List:
-                return List(arguments[0].array)
-            elif type(obj) == str:
-                return List(list(arguments[0]))
-            elif type(obj) == float:
-                length = int(obj)
-                array = list()
-                for i in range(0, length):
-                    array.append(None)
-                return List(array)
+            if len(arguments) > 0:
+                obj = arguments[0]
+                if type(obj) == List:
+                    return List(arguments[0].array)
+                elif type(obj) == str:
+                    return List(list(arguments[0]))
+                elif type(obj) == float:
+                    length = int(obj)
+                    array = list()
+                    for i in range(0, length):
+                        array.append(None)
+                    return List(array)
+            else:
+                return List([])
 
     def arity(self):
-        return 1
+        return [0,1]
 
 initList = ListInit()
